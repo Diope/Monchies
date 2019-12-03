70b9cfb5a4fdda6d5e48e7030e25cfe40ac18180
Dion Pettaway <dionp@fullsail.edu>
2019-12-02 22:03:40 -0500

    Initial Commit

---
+++ Food Spottin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Spottin
+//
+//  Created by Dion Pettaway on 12/2/19.
+//  Copyright © 2019 Dipet_Low. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
